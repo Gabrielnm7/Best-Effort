5669a3e90d16ed7752eeda7f0041efeb58c363df
Martin Cuestas <165968711+martincuestas@users.noreply.github.com>
2024-11-02 16:54:48 -0300

    Agrego ComparadorPorGanancias

---
+++ tp2/src/main/java/aed/ComparadorPorGanancias.java
@@ -0,0 +1,24 @@
+package aed;
+import java.util.Comparator;
+
+public class ComparadorPorGanancias implements Comparator<Traslado>  {
+  @Override
+ 
+  public int compare(Traslado a, Traslado b){
+    if (a.gananciaNeta > b.gananciaNeta){
+        return 1;
+    }
+    else if (a.gananciaNeta < b.gananciaNeta){
+        return (-1);
+    }
+    else{
+        if (a.id < b.id){
+            return 1;
+        }
+        else {
+            return (-1);
+        }
+        }
+    }
+    }
+  
