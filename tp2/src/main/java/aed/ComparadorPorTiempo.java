8f58ec36b1b1aed0db304522e40cedc2ec7b8df4
Martin Cuestas <165968711+martincuestas@users.noreply.github.com>
2024-11-02 16:55:12 -0300

    Agrego ComparadorPorTiempo

---
+++ tp2/src/main/java/aed/ComparadorPorTiempo.java
@@ -0,0 +1,26 @@
+package aed;
+import java.util.Comparator;
+
+
+public class ComparadorPorTiempo implements Comparator<Traslado> {
+    @Override
+    public int compare(Traslado a, Traslado b){
+        if (a.timestamp > b.timestamp){
+            return 1;
+        }
+        else if (a.timestamp < b.timestamp){
+            return (-1);
+        }
+        else{
+            if (a.id < b.id){
+                return 1;
+            }
+            else {
+                return (-1);
+            }
+            }
+        }
+
+
+    }
+    
