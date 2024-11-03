9c55b202957a3942bf6c8fb5be6583c46c001297
Martin <martincuestas51@gmail.com>
2024-11-03 18:13:19 -0300

    Agrego varios metodos a class ColaDePrioridad

--- tp2/src/main/java/aed/ColaDePrioridad.java
+++ tp2/src/main/java/aed/ColaDePrioridad.java
@@ -1,19 +1,22 @@
 package aed;
 
 import java.util.ArrayList;
+import java.util.Comparator;
  
-public class ColaDePrioridad implements ColaPrioridad<Traslado> {                    // VAMOS A IMPLEMETARLO CON UN ARRAY YA QUE DICEN QUE ES LA MEJOR FORMA / ME CONVIENE ARRAY O ARRAY LIST?
-    private ArrayList<Traslado> datos;
+public class ColaDePrioridad<T> implements ColaPrioridad<T> {                    // VAMOS A IMPLEMETARLO CON UN ARRAY YA QUE DICEN QUE ES LA MEJOR FORMA / ME CONVIENE ARRAY O ARRAY LIST?
+    private ArrayList<T> datos;
+    private Comparator<T> comparador;
     
-    public ColaDePrioridad(){
+    public ColaDePrioridad(Comparator<T> c){
         this.datos = new ArrayList<>();
+        this.comparador = c;
     }
 
     public boolean vacía(){
         return (this.datos.size() == 0);
     }
 
-    public void encolar(Traslado t){                // O(log n) ya que utilizo la funcion subir y add es O(1) para este TP
+    public void encolar(T t){                // O(log n) ya que utilizo la funcion subir y add es O(1) para este TP
         this.datos.add(t);
         this.subir(this.datos.size()-1);
 
@@ -21,26 +24,64 @@ public class ColaDePrioridad implements ColaPrioridad<Traslado> {               
 
     private void subir(int indice){
         int i = indice;
-        ComparadorPorGanancias comparador = new ComparadorPorGanancias();
         while (i != 0 && (comparador.compare(datos.get(i), (datos.get((i - 1)/2)))) > 0 ){         // TENEMOS QUE VER BIEN COMO ES LO DE COMPARAR ya que estoy comparando solamente con solo un parametro en este caso
-            Traslado ultimo = datos.get(i);
-            datos.set(i, datos.get((i-1)/2));                                       // Esta operación es O(log n) ya que no debo recorrer todos los elementos del arbol.
-            datos.set(((i-1)/2), ultimo);
+            T ultimo = datos.get(i);
+           datos.set(i, datos.get(( i-1)/2));                                       // Esta operación es O(log n) ya que no debo recorrer todos los elementos del arbol.
+            datos.set(((i-1)/2), ultimo);       
             i = (i-1)/2; 
         }
     
     }
 
-    public Traslado desencolarMax();
-    // Saca el primer elemento (El de mayor prioridad) y reordena para mantener el Invariante de representación.
+    public T desencolarMax(){
+        T valor = datos.get(0);
+        datos.set(0, datos.get(datos.size()));
+        datos.remove(datos.size()-1);
+        this.bajar(0);
+        return valor;
 
-    public void cambiarPrioridad (int i, Traslado prioridad);
-    // Cambia la prioridad del elemento en la posicion i y reordena para mantener el Invariante de representación.
+    }
 
-    public Traslado consultarMax();
-    // Devuelve el elemento de máxima prioridad sin cambiar nada.
+    private void bajar(int indice){
+        int i = indice;
+        while (!EsHoja(i) && (comparador.compare(datos.get(i*2+1),datos.get(i)) > 0 || comparador.compare(datos.get(i*2+2),datos.get(i)) > 0)){
+            if (comparador.compare(datos.get(i*2+1), datos.get(i*2+2)) > 0){   // LO CAMBIO POR EL HIJO MAS GRANDE
+                T valor = datos.get(i);
+                datos.set(i, datos.get(i*2+1));
+                datos.set(2*1+1, valor);
+                i = i*2 + 1;
+            }
+            else if (comparador.compare(datos.get(i*2+1), datos.get(i*2+2)) < 0){
+                T valor = datos.get(i);
+                datos.set(i, datos.get(i*2+2));
+                datos.set(2*1+2, valor);
+                i = i*2 + 2;
+            }
 
-    public ColaPrioridad<Traslado> ColaDePrioridadDesdeSecuencia(Traslado[] secuencia);
+        }
+
+    }
+
+    private boolean EsHoja(int indice){
+        return ((indice * 2 + 1) >= datos.size() && indice * 2 + 2 >= datos.size());
+    }
+    
+
+    public void cambiarPrioridad (int i, T prioridad){
+    T valorAnterior = datos.get(i);
+        datos.set(i, prioridad);
+    if (comparador.compare (valorAnterior, prioridad) < 0){   // Reordeno segun si es mayor o menos al elemento anterior
+        subir(i);
+    }
+    else{
+        bajar(i);
+    }
+    }
+
+    public T consultarMax(){
+    return datos.get(0);}
+
+    public ColaDePrioridad ColaDePrioridadDesdeSecuencia(T[] secuencia);
     // Agarra una secuencia la reordena y la convierte en un tipo ColaPrioridad.
 
 
