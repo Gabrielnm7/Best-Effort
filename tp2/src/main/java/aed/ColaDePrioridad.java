c0cdb993dc00b78ff4dc41f7bce48c08190683cf
Martin <martincuestas51@gmail.com>
2024-11-06 15:30:41 -0300

    ColaDePrioridad terminada

--- tp2/src/main/java/aed/ColaDePrioridad.java
+++ tp2/src/main/java/aed/ColaDePrioridad.java
@@ -1,35 +1,22 @@
 package aed;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
 import java.util.Comparator;
  
 public class ColaDePrioridad<T> implements ColaPrioridad<T> {                    // VAMOS A IMPLEMETARLO CON UN ARRAY YA QUE DICEN QUE ES LA MEJOR FORMA / ME CONVIENE ARRAY O ARRAY LIST?
     private ArrayList<T> datos;
     private Comparator<T> comparador;
     
-    public ColaDePrioridad(Comparator<T> c){
-=======
 
-public class ColaDePrioridad implements ColaPrioridad<Traslado> { // VAMOS A IMPLEMETARLO CON UN ARRAY YA QUE DICEN QUE
-                                                                  // ES LA MEJOR FORMA / ME CONVIENE ARRAY O ARRAY LIST?
-    private ArrayList<Traslado> datos;
-
-    public ColaDePrioridad() {
->>>>>>> a15bf663fd58527bfb5100da84edc2cf2b06ce00
+    public ColaDePrioridad(Comparator<T> c) {     //O(1) tan solo limitadas operaciones elementales sin ciclos y sin depender de la entrada.
         this.datos = new ArrayList<>();
         this.comparador = c;
     }
 
-    public boolean vacía() {
+    public boolean vacía() {                // O(1) pues la operación size es O(1)
         return (this.datos.size() == 0);
     }
-
-<<<<<<< HEAD
-    public void encolar(T t){                // O(log n) ya que utilizo la funcion subir y add es O(1) para este TP
-=======
-    public void encolar(Traslado t) { // O(log n) ya que utilizo la funcion subir y add es O(1) para este TP
->>>>>>> a15bf663fd58527bfb5100da84edc2cf2b06ce00
+    public void encolar(T t) { // O(log n) ya que utilizo la funcion subir que es O(log n), y add es O(1) para este TP
         this.datos.add(t);
         this.subir(this.datos.size() - 1);
 
@@ -37,27 +24,15 @@ public class ColaDePrioridad implements ColaPrioridad<Traslado> { // VAMOS A IMP
 
     private void subir(int indice) {
         int i = indice;
-<<<<<<< HEAD
-        while (i != 0 && (comparador.compare(datos.get(i), (datos.get((i - 1)/2)))) > 0 ){         // TENEMOS QUE VER BIEN COMO ES LO DE COMPARAR ya que estoy comparando solamente con solo un parametro en este caso
+        while (i != 0 && (comparador.compare(datos.get(i), (datos.get((i - 1)/2)))) > 0 ){         
             T ultimo = datos.get(i);
-           datos.set(i, datos.get(( i-1)/2));                                       // Esta operación es O(log n) ya que no debo recorrer todos los elementos del arbol.
+           datos.set(i, datos.get(( i-1)/2));                       // Esta operación es O(log n) ya que no debo recorrer todos los elementos del arbol, y la altura es logaritmica respecto a la cantidad de nodos.
             datos.set(((i-1)/2), ultimo);       
             i = (i-1)/2; 
-=======
-        ComparadorPorGanancias comparador = new ComparadorPorGanancias();
-        while (i != 0 && (comparador.compare(datos.get(i), (datos.get((i - 1) / 2)))) > 0) {
-            Traslado ultimo = datos.get(i);
-            datos.set(i, datos.get((i - 1) / 2)); // Esta operación es O(log n) ya que no debo recorrer todos los
-                                                  // elementos del arbol.
-            datos.set(((i - 1) / 2), ultimo);
-            i = (i - 1) / 2;
->>>>>>> a15bf663fd58527bfb5100da84edc2cf2b06ce00
-        }
 
-    }
+    }}
 
-<<<<<<< HEAD
-    public T desencolarMax(){
+    public T desencolarMax(){                //O(1), debido a que el máximo siempre sera el primer elemento de la secuencia, entonces necesito limitadas operaciones elementales O(1)
         T valor = datos.get(0);
         datos.set(0, datos.get(datos.size()));
         datos.remove(datos.size()-1);
@@ -66,7 +41,7 @@ public class ColaDePrioridad implements ColaPrioridad<Traslado> { // VAMOS A IMP
 
     }
 
-    private void bajar(int indice){
+    private void bajar(int indice){         // O(n) por las mismas rázones de subir.
         int i = indice;
         while (!EsHoja(i) && (comparador.compare(datos.get(i*2+1),datos.get(i)) > 0 || comparador.compare(datos.get(i*2+2),datos.get(i)) > 0)){
             if (comparador.compare(datos.get(i*2+1), datos.get(i*2+2)) > 0){   // LO CAMBIO POR EL HIJO MAS GRANDE
@@ -86,12 +61,12 @@ public class ColaDePrioridad implements ColaPrioridad<Traslado> { // VAMOS A IMP
 
     }
 
-    private boolean EsHoja(int indice){
-        return ((indice * 2 + 1) >= datos.size() && indice * 2 + 2 >= datos.size());
+    private boolean EsHoja(int indice){                                               // O(1) debido a que son limitadas operaciones elementales O(1)
+        return ((indice * 2 + 1) >= datos.size() && indice * 2 + 2 >= datos.size());  
     }
     
 
-    public void cambiarPrioridad (int i, T prioridad){
+    public void cambiarPrioridad (int i, T prioridad){          // O(log n), porque por ahi tengo que bajar o subir el elemento, operaciones que son O(log n)
     T valorAnterior = datos.get(i);
         datos.set(i, prioridad);
     if (comparador.compare (valorAnterior, prioridad) < 0){   // Reordeno segun si es mayor o menos al elemento anterior
@@ -105,33 +80,27 @@ public class ColaDePrioridad implements ColaPrioridad<Traslado> { // VAMOS A IMP
     public T consultarMax(){
     return datos.get(0);}
 
-    public ColaDePrioridad ColaDePrioridadDesdeSecuencia(T[] secuencia);
-=======
-    // Saca el primer elemento (El de mayor prioridad) y reordena para mantener el
-    // Invariante de representación.
-    public Traslado desencolarMax() {
-        // Implementar
-        throw new UnsupportedOperationException("No implementada aun");
-    };
+    private ArrayList<T> ArrayASecuencia (T[] arreglo){         // O(n) porque tengo que recorrer la lista elemento por elemento y realizar una operación O(1)
+       ArrayList<T> res = new ArrayList<>();
+        int i = 0;
+        while (i < arreglo.length){
+            res.add(arreglo[i]);
+            i ++;
+        }
+        return res;
+    }
 
-    // Cambia la prioridad del elemento en la posicion i y reordena para mantener el
-    // Invariante de representación.
-    public void cambiarPrioridad(int i, Traslado prioridad) {
-        // Implementar
-        throw new UnsupportedOperationException("No implementada aun");
-    };
+    public ColaDePrioridad<T> ColaDePrioridadDesdeSecuencia(T[] secuencia){        // ALGORITMO DE FLOYD HEAPIFY O(n) por alguna razón.
+        
+        ColaDePrioridad<T> res = new ColaDePrioridad<>(comparador);
+        int i = 0;
+        ArrayList<T> data = ArrayASecuencia(secuencia); // O(n)
+        res.datos = data;
+        while (i < data.size()){  //O(n)
+            res.bajar(data.size() - 1 - i); // Gran parte de las veces bajar no hace nada porque son hojas o ya cumple el invariante, lo que seria O(1) por no entrar al ciclo.
+        }
+        return res;
 
-    // Devuelve el elemento de máxima prioridad sin cambiar nada.
-    public Traslado consultarMax() {
-        // Implementar
-        throw new UnsupportedOperationException("No implementada aun");
-    };
-
->>>>>>> a15bf663fd58527bfb5100da84edc2cf2b06ce00
-    // Agarra una secuencia la reordena y la convierte en un tipo ColaPrioridad.
-    public ColaPrioridad<Traslado> ColaDePrioridadDesdeSecuencia(Traslado[] secuencia) {
-        // Implementar
-        throw new UnsupportedOperationException("No implementada aun");
-    };
+    }
 
 }
