bb04d8452d47876c4788abfd28b4c599dd795343
Geronimo Pacheco Parrondo <pachecogero16@gmail.com>
2024-11-14 02:26:23 -0300

    Agrego casos de test

---
+++ tp2/src/test/java/aed/ColaDePrioridadTest.java
@@ -0,0 +1,127 @@
+package aed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Array;
+import java.util.Comparator;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ColaDePrioridadTest {
+
+    Traslado t1;
+    Traslado t2;
+    Traslado t3;
+    Traslado t4;
+    Traslado t5;
+    Traslado t6;
+
+    @BeforeEach
+    void init(){
+        t1 = new Traslado(1, 2, 3, 4, 5);
+        t2 = new Traslado(11, 3, 2, 200, 7);
+        t3 = new Traslado(3, 3, 1, 4, 5);
+        t4 = new Traslado(34, 1, 2, 300, 10);
+        t5 = new Traslado(10, 2, 1, 200, 5);
+        t6 = new Traslado(9, 2, 1, 200, 5);
+    }
+    ComparadorPorGanancias c1 = new ComparadorPorGanancias();
+    ComparadorPorTiempo c2 = new ComparadorPorTiempo();
+    ColaDePrioridad<Traslado> colaGasto = new ColaDePrioridad<Traslado>(c1);
+    ColaDePrioridad<Traslado> colaTimeStamp = new ColaDePrioridad<Traslado>(c2);
+
+
+    //Comparator<> c = new Comparator<T>();
+
+
+    @Test
+    void nueva_cola_vacia(){
+        assertEquals(true, colaGasto.vacía());
+        assertEquals(true, colaTimeStamp.vacía());
+    }
+
+    @Test
+    void encolar_elemento(){
+        colaGasto.encolar(t1);
+        colaTimeStamp.encolar(t1);
+        assertEquals(t1, colaGasto.consultarMax());
+        assertEquals(t1, colaTimeStamp.consultarMax());
+        assertEquals(false, colaGasto.vacía());
+        assertEquals(false, colaTimeStamp.vacía());
+
+    }
+
+    @Test
+    void desencolar_maximo(){
+        colaGasto.encolar(t1);
+        colaTimeStamp.encolar(t1);
+        colaGasto.encolar(t2);
+        colaTimeStamp.encolar(t2);
+        colaGasto.encolar(t3);
+        colaTimeStamp.encolar(t3);
+
+        assertEquals(t2, colaGasto.consultarMax());
+        assertEquals(t1, colaTimeStamp.consultarMax());
+
+        colaGasto.desencolarMax();
+        colaTimeStamp.desencolarMax();
+     //   assertEquals(2, cola1.datos.size());
+      //  assertEquals(3, cola2.datos.size());
+       // assertEquals(2, cola2.datos.size());
+      //  assertEquals(false, cola1.vacía());
+      //  assertEquals(false, cola2.vacía());
+        assertEquals(t1, colaGasto.consultarMax());
+        assertEquals(t3, colaTimeStamp.consultarMax());
+
+        colaGasto.desencolarMax();
+        colaTimeStamp.desencolarMax();
+
+        assertEquals(t3, colaGasto.consultarMax());
+        assertEquals(t2, colaTimeStamp.consultarMax());
+    } 
+
+    @Test
+    void desencolar_mas_dificil(){
+        colaGasto.encolar(t1);
+        colaTimeStamp.encolar(t1);
+        colaGasto.encolar(t2);
+        colaTimeStamp.encolar(t2);
+        colaGasto.encolar(t3);
+        colaTimeStamp.encolar(t3);
+        colaGasto.encolar(t4);
+        colaTimeStamp.encolar(t4);
+        colaGasto.encolar(t5);
+        colaTimeStamp.encolar(t5);
+        colaGasto.encolar(t6);
+        colaTimeStamp.encolar(t6);
+
+        assertEquals(t4, colaGasto.consultarMax());
+        assertEquals(t1, colaTimeStamp.consultarMax());
+
+        colaGasto.desencolarMax();
+        colaTimeStamp.desencolarMax();
+
+        assertEquals(t6, colaGasto.consultarMax());
+        assertEquals(t3, colaTimeStamp.consultarMax());
+
+        colaGasto.desencolarMax();
+        colaTimeStamp.desencolarMax();
+
+        assertEquals(t5, colaGasto.consultarMax());
+        assertEquals(t6, colaTimeStamp.consultarMax());
+
+        colaGasto.desencolarMax();
+        colaTimeStamp.desencolarMax();
+
+        assertEquals(t2, colaGasto.consultarMax());
+        assertEquals(t5, colaTimeStamp.consultarMax());
+
+        colaGasto.desencolarMax();
+        colaTimeStamp.desencolarMax();
+
+        assertEquals(t1, colaGasto.consultarMax());
+        assertEquals(t2, colaTimeStamp.consultarMax());
+    }
+
+}
---
+++ tp2/src/test/java/aed/ComparadorTest.java
@@ -0,0 +1,76 @@
+package aed;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ComparadorTest {
+    Traslado t1;
+    Traslado t2;
+    Traslado t3;
+    Traslado t4;
+    Traslado t5;
+  //  int cantCiudades;
+   // Traslado[] listaTraslados;
+   // ArrayList<Integer> actual;
+
+    @BeforeEach
+ //   void init(){
+        //Reiniciamos los valores de las ciudades y traslados antes de cada test
+   //     cantCiudades = 7;
+     //   listaTraslados = new Traslado[] {
+       //                                     new Traslado(8, 1, 2, 300, 15),
+         //                                   new Traslado(9, 2, 1, 400, 10)
+           //                             };
+    void init(){
+        t1 = new Traslado(8, 1, 2, 400, 15);
+        t2 = new Traslado(9, 2, 1, 300, 10);
+        t3 = new Traslado(5, 1, 2, 300, 10);
+        t4 = new Traslado(34, 1, 2, 300, 10);
+        t5 = new Traslado(10, 2, 1, 200, 5);
+    }
+
+    ComparadorPorTiempo c1 = new ComparadorPorTiempo();
+
+    @Test
+    void primero_mayor_timestamp(){
+        assertEquals(-1, c1.compare(t1,t2));
+    }
+
+    @Test
+    void primero_menor_id(){
+        assertEquals(1, c1.compare(t3,t4));
+    }
+
+    @Test
+    void primero_mayor_id(){
+        assertEquals(-1, c1.compare(t2,t3));
+    }
+
+    @Test
+    void primero_menor_timestamp(){
+        assertEquals(1, c1.compare(t5,t4));
+    }
+
+    ComparadorPorGanancias c2 = new ComparadorPorGanancias();
+
+    @Test
+    void primero_mayor_ganancia(){
+        assertEquals(1, c1.compare(t5,t4));
+    }
+
+    @Test
+    void primero_menor_id_2(){
+        assertEquals(1, c1.compare(t3,t4));
+    }
+
+    @Test
+    void primero_mayor_id_2(){
+        assertEquals(-1, c1.compare(t2,t3));
+    }
+
+    @Test
+    void primero_menor_ganancia(){
+        assertEquals(1, c1.compare(t5,t4));
+    }
+}
